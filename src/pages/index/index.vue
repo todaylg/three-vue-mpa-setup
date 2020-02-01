f4af4b3dc999c599abc0d54b634d53f08d9524a6
todaylg <lg2lugang@gmail.com>
2020-02-01 23:22:24 +0800

    Init

---
+++ src/pages/index/index.vue
@@ -0,0 +1,43 @@
+<template>
+<div id="wrapper">
+	<div id="canvasContainer"></div>
+</div>
+</template>
+
+<script>
+export default {
+	name: 'container',
+	mounted() {
+    },
+    methods: {
+    }
+};
+</script>
+
+<style lang="less">
+html,
+body {
+	margin: 0;
+	padding: 0;
+	box-sizing: border-box;
+	background: #000000;
+}
+#wrapper {
+	font-family: 'Avenir', Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+}
+#wrapper::-webkit-scrollbar {
+	display: none;
+}
+#wrapper{
+    width: 100vw;
+    height: 100vh;
+    position: relative;
+	overflow: hidden;
+}
+#canvasContainer {
+    width: 100%;
+    height: 100%;
+}
+</style>
